e53b35658430c5d0dde0bf39b58c5340c5babc1f
Anshuman1803 <mahianshuman491@gmail.com>
2024-09-27 22:46:06 +0530

    update the admin section and also the event section

--- pages/api/addeve.ts
+++ pages/api/addeve.ts
@@ -9,34 +9,46 @@ type Data = {
 
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse,
+  res: NextApiResponse
 ) {
-    
-        await mongoose.connect('mongodb+srv://Nilesh:9319601021@mongoyt.ubxb7fr.mongodb.net/devapex')
-    console.log(req.body)
+  await mongoose.connect(
+    "mongodb+srv://Nilesh:9319601021@mongoyt.ubxb7fr.mongodb.net/devapex"
+  );
 
-    if(req.method == 'POST'){
-        const name = req.body.name
-        const date = req.body.date
-        const venue = req.body.venue
-        const imgurl = req.body.imgurl
-        const link = req.body.link
+  if (req.method == "POST") {
+    const name = req.body.name;
+    const date = req.body.date;
+    const venue = req.body.venue;
+    const imgurl = req.body.imgurl;
+    const link = req.body.link;
+    const category = req.body.category;
 
-        let p = new event({
-                    name : name,
-                    date : date,
-                    venue : venue,
-                    imgurl : imgurl,
-                    link : link
-                })
-        await p.save();
+    let p = new event({
+      name: name,
+      date: date,
+      venue: venue,
+      imgurl: imgurl,
+      link: link,
+      category: category,
+    });
+    await p.save();
 
-        res.status(200).json({ success: true });
+    res.status(200).json({ success: true });
+  }
+
+  if (req.method == "GET") {
+    const posts = await event.find();
+    res.status(200).json({ success: true, data: posts });
+  }
+
+  if (req.method == "DELETE") {
+    const { eventID } = req.query;
+    const deleteRes = await event.findOneAndDelete({ _id: eventID });
+    if (deleteRes) {
+      res.status(200).json({ success: true });
+    } else {
+      res.status(404).json({ success: false });
     }
-
-    if(req.method == 'GET'){
-        const posts = await event.find();
-        res.status(200).json({ success: true , data: posts});
-    }
+  }
   
 }
