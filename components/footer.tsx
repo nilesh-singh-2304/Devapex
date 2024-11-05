56b8772a9bf8c436f36429823455c5f14aea4fc7
nilesh-singh-2304 <shyaam2304@gmail.com>
2024-11-06 02:31:50 +0530

    new

--- components/footer.tsx
+++ components/footer.tsx
@@ -8,8 +8,8 @@ const Footer = () => {
       <a href="#" className="flex justify-center items-center text-2xl font-semibold text-gray-900 dark:text-white">
           <img src="https://www.imgtr.net/ib/s5l2riCMd1UNZj5_1730748864.png" className='h-20' alt="" />    
       </a>
-      <p className="my-6 text-gray-500 dark:text-gray-400">Your Ultimate Coding Hub</p>
-      <ul className="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
+      <p className="my-6 text-gray-500 dark:text-gray-400">Harnessing Talent Driving Innvation</p>
+      {/* <ul className="flex flex-wrap justify-center items-center mb-6 text-gray-900 dark:text-white">
           <li>
               <a href="#" className="mr-4 hover:underline md:mr-6 ">About</a>
           </li>
@@ -31,9 +31,9 @@ const Footer = () => {
           <li>
               <a href="#" className="mr-4 hover:underline md:mr-6">Contact</a>
           </li>
-      </ul>
-      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024-2025 <a href="#" className="hover:underline">Devapex™</a>. All Rights Reserved.</span>
-      <p className='text-xs text-gray-500 mt-1 sm:text-center dark:text-gray-400'>Special Thanks to Nilesh Singh & Lavish</p>
+      </ul> */}
+      <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2024-2025 <a href="#" className="hover:underline">StarkSeek™</a>. All Rights Reserved.</span>
+      <p className='text-xs text-gray-500 mt-1 sm:text-center dark:text-gray-400'>Credits : Nilesh Singh</p>
   </div>
 </footer>
   )
--- pages/index.tsx
+++ pages/index.tsx
@@ -25,7 +25,7 @@ export default function Home({course}: any) {
     <div className="max-w-[85rem] mx-auto px-4 sm:px-6 lg:px-8 py-10 lg:py-16">
       <div className="max-w-2xl text-center mx-auto">
         <p className="inline-block text-sm font-medium bg-clip-text bg-gradient-to-l from-blue-600 to-violet-500 text-transparent dark:from-blue-400 dark:to-violet-400">
-          Ultimate Coding Hub 
+        Harnessing Talent Driving Innvation
         </p>
 
         
